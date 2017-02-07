d5a2181860f6002859fe193692a0da8d5da682ef
Sonia Raychaudhuri <sonia.raychaudhuri@ericsson.com>
2017-02-07 17:40:08 +0530

    Manage Products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
+selectedProduct={};
+
+  product1 = {
+    name: "RCT1",
+    description: "RCT 1",
+    type: "Ruby",
+    quantity: 1000
+  };
+  product2 = {
+    name: "RCT2",
+    description: "RCT 2",
+    type: "Ruby",
+    quantity: 5500
+  };
+
+  productList = [this.product1, this.product2];
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.productList);
+  }
+
+  editProduct(product: string) {
+    console.log("Product edited:", product);
+    this.selectedProduct = product;
+  }
+
+  onSubmit(form: NgForm) {
+    console.log("Form submitted...");
+    console.log(form.value);
+  }
+
+  resetForm(form: NgForm) {
+    form.reset();
   }
 
 }
