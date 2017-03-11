05185a15615ac129ed202e0bda30e7b2f3170921
rahul aggarwal <rahul.a.aggarwal@ericsson.com>
2017-03-11 13:15:58 +0530

    Merge conflict resolved

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,12 +1,8 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Product } from './product';
-<<<<<<< Updated upstream
 import {ProductImage} from './productImage';
 import {ProductService} from '../services/product.service';
-=======
-import {ProductService} from '../services/product.service'
->>>>>>> Stashed changes
 
 @Component({
   selector: 'app-products',
@@ -15,10 +11,8 @@ import {ProductService} from '../services/product.service'
 })
 export class ProductsComponent implements OnInit {
 
-
 selectedProduct = {};
 formShow = "hidden";
-<<<<<<< Updated upstream
 formImageShow = "hidden";
 showClass = "show";
 hideClass = "hidden";
@@ -69,27 +63,6 @@ productImageJson = {
         },
         error => console.log(error)
       );
-   // this.getProductMasterData();
-=======
-productList = [];
-
-  //product1: Product = new Product("RCT1","RCT 1","Ruby",1000);
-  //product2: Product = new Product("RCT2","RCT 2","Ruby",7080);
-  
-  //productList = [this.product1, this.product2];
-
-    constructor(private productService: ProductService) {}
-
-
-  ngOnInit() {
-
-      this.productService.getProductData()
-      .subscribe(
-        response => this.productList = response,
-        error => console.log(error)
-      );
->>>>>>> Stashed changes
-    
     console.log(this.productList);
   }
 
